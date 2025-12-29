185fd924558d59f7e0313400d63054ddc57cdd1c
uday <dunna.udaykiran1729@gmail.com>
2025-12-29 18:39:05 +0530

    second _commit

--- Ai_backend/server.js
+++ Ai_backend/server.js
@@ -1,34 +1,35 @@
-const express = require("express")
-const cors = require("cors")
-require("dotenv").config();
+require("dotenv").config(); 
+
+const express = require("express");
+const cors = require("cors");
 const { Assistant } = require("./assistant/googleai");
 
-const app = express()
+const app = express();
 
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json())
-app.use(cors())
 const assistant = new Assistant();
-
 
 app.post("/api/chat", async (req, res) => {
   const { content } = req.body;
 
-  if (!content) {
-     res.send("Message is required");
-  }
+  // if (!content) {
+  //   return res.status(400).json({ error: "Message is required" });
+  // }
 
   try {
     const reply = await assistant.chat(content);
     res.json({ reply });
   } catch (error) {
-    console.error(error);
+    console.error("Backend Error:", error);
+    
     res.json({status:false,
-        message:error
+      error:error
     })
   }
 });
 
-app.listen("1321",()=>{
-    console.log("Started")
-})
+app.listen(1321, () => {
+  console.log("Started");
+});
